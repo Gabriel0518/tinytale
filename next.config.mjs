ea5ec4265f6c4e0dd95a2db21cab993457f7689e
Gabriel0518 <151370697+Gabriel0518@users.noreply.github.com>
2026-03-08 14:52:13 +0800

    fix: add production API URL to CSP connect-src

--- next.config.mjs
+++ next.config.mjs
@@ -64,7 +64,7 @@ const nextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com",
               "img-src 'self' data: blob: https: http:",
               "font-src 'self' data: https://fonts.gstatic.com",
-              "connect-src 'self' https://api.tinytale.top https://bucket.tinytale.top https://*.cloudflarestream.com https://accounts.google.com https://cloudflareinsights.com",
+              "connect-src 'self' http://93.188.160.112:7002 https://api.tinytale.top https://bucket.tinytale.top https://*.cloudflarestream.com https://accounts.google.com https://cloudflareinsights.com",
               "media-src 'self' blob: https://*.cloudflarestream.com",
               "frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://accounts.google.com",
             ].join('; '),
